aefc82539a3b6c5ad1a9ceb5318f50fce564b1bb
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-21 13:34:10 +0530

    react navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome React Native</Text>
+      <Link href="/login">Login Page</Link>
     </View>
   );
 }
---
+++ app/login.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const LoginScreen = () => {
+  return <View>Login Screen</View>;
+};
+
+export default LoginScreen;
