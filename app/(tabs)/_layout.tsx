e68b17a85920b9ddb5e9eb0b75b52e9358f39e8e
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-21 14:57:20 +0530

    tabBar icon active and inactive

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,39 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="login" options={{ title: "Login" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "coral",
+        tabBarActiveBackgroundColor: "white",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <FontAwesome5 name="home" size={24} color={color} />
+            ) : (
+              <AntDesign name="home" size={24} color={color} />
+            ),
+        }}
+      />
+      <Tabs.Screen
+        name="login"
+        options={{
+          title: "Login",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? (
+              <FontAwesome5 name="user" size={24} color={color} />
+            ) : (
+              <AntDesign name="user" size={24} color={color} />
+            ),
+        }}
+      />
     </Tabs>
   );
 }
