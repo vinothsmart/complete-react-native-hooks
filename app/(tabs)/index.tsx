c45146200580cc5cd6715544f7a1672c91ddb7ae
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-21 19:44:41 +0530

    updated

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -30,7 +30,6 @@ const styles = StyleSheet.create({
     textAlign: "center",
     lineHeight: 20,
   },
-
   welcomeText: {
     fontSize: 20,
     marginBottom: 20,
--- app/auth.tsx
+++ app/auth.tsx
@@ -1,26 +1,66 @@
 import { useState } from "react";
-import { KeyboardAvoidingView, Platform, View } from "react-native";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 import { Button, Text, TextInput } from "react-native-paper";
 
 export default function AuthScreen() {
-  const [isSingup, setIsSignup] = useState();
+  const [isSingup, setIsSignup] = useState<boolean>(false);
+  const [userDetails, setUserDetails] = useState<{
+    email: string;
+    password: string;
+  }>({
+    email: "",
+    password: "",
+  });
+
+  const handleAuth = async () => {
+    // setUserDetails((prev) => ({
+    //   ...prev,
+    // }));
+  };
+
+  const handleInputChange =
+    (key: keyof typeof userDetails) => (value: string) => {
+      setUserDetails((prev) => ({
+        ...prev,
+        [key]: value,
+      }));
+    };
+
+  const handleSwitchMode = () => {
+    setIsSignup((prev) => !prev);
+  };
+
+  console.log(userDetails);
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
     >
-      <View>
-        <Text>{isSingup ? "Create Account" : "Welcome Back"}</Text>
+      <View style={styles.content}>
+        <Text style={styles.title} variant="headlineMedium">
+          {isSingup ? "Create Account" : "Welcome Back"}
+        </Text>
         <TextInput
+          style={styles.input}
           label="Email"
           autoCapitalize="none"
           placeholder="example@gmail.com"
           keyboardType="email-address"
           mode="outlined"
+          onChangeText={handleInputChange("email")}
         />
-        <TextInput label="Password" autoCapitalize="none" mode="outlined" />
-        <Button mode="contained">{isSingup ? "Sign Up" : "Sign In"}</Button>
-        <Button mode="text">
+        <TextInput
+          style={styles.input}
+          label="Password"
+          autoCapitalize="none"
+          mode="outlined"
+          onChangeText={handleInputChange("password")}
+        />
+        <Button style={styles.button} mode="contained">
+          {isSingup ? "Sign Up" : "Sign In"}
+        </Button>
+        <Button style={styles.switchModeButton} mode="text">
           {isSingup
             ? "Already have an account? Sign In"
             : "Don't have an account? Sign Up"}
@@ -29,3 +69,28 @@ export default function AuthScreen() {
     </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
